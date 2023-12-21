61275e00dbd32534cdc2630c23117385851cda3d
Md.Abir hossain <abir2hossain2084@gmail.com>
2023-12-21 21:33:25 +0600

    animation added in services

--- src/Pages/Home/About.jsx
+++ src/Pages/Home/About.jsx
@@ -5,11 +5,31 @@ import 'aos/dist/aos.css';
 
 const About = () => {
     useEffect(() => {
-        AOS.init({ duration: 1000, once: true }); // Initialize AOS
+        AOS.init({ duration: 1000, once: true });
+
+        const handleScroll = () => {
+            const element = document.getElementById('about');
+            if (element) {
+                const rect = element.getBoundingClientRect();
+                const isVisible = rect.top <= window.innerHeight && rect.bottom >= 0;
+
+                if (isVisible) {
+                    AOS.refreshHard(); // Use refreshHard to force AOS to consider elements in view
+                    AOS.refresh();
+                }
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
     }, []);
 
     return (
         <motion.div
+            id="about"
             className="h-96 my-20 "
             style={{
                 backgroundImage: `url(${'https://i.ibb.co/nQ3ZT4S/alex-kotliarskyi-QBp-ZGq-EMs-Kg-unsplash.jpg'})`,
@@ -19,21 +39,33 @@ const About = () => {
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-            data-aos="fade-up" // Apply AOS fade-up effect
+            data-aos="fade-up"
         >
             <motion.div
                 className="hero-content text-center rounded-lg text-[#fee3e2] glass bg-transparent relative top-1/4 mx-20 h-1/2 "
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.5 }}
             >
                 <div className="max-w-md">
-                    <h1 className="text-5xl text-[#fee3e2] font-bold">Task Leaf</h1>
-                    <p className="py-6">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus
-                        laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore
-                        at, nihil iusto ducimus incidunt quibusdam nemo.
-                    </p>
+                    <motion.h1
+                        className="text-5xl text-[#fee3e2] font-bold"
+                        initial={{ opacity: 0, x: -50 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5, delay: 0.2 }}
+                    >
+                        Task Leaf
+                    </motion.h1>
+                    <motion.p
+                        className="py-6"
+                        initial={{ opacity: 0, x: -50 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5, delay: 0.4 }}
+                    >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum
+                        deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil
+                        iusto ducimus incidunt quibusdam nemo.
+                    </motion.p>
                 </div>
             </motion.div>
         </motion.div>
--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -1,70 +1,71 @@
-
+import React, { useEffect } from 'react';
+import { motion } from 'framer-motion';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
+import { FreeMode, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
-
-
-
-// import required modules
-import { FreeMode, Pagination } from 'swiper/modules';
-
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
 
 const Services = () => {
-    return (
-        <>
-      <section>
-        <SectionTitle 
-        subheading={'---Find your service---'}
-          heading={'Our Users '}
-        > </SectionTitle>
-        
-      <Swiper
-          slidesPerView={4}
-          spaceBetween={10}
-          freeMode={true}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[FreeMode, Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <img className='' src={'https://i.ibb.co/t8wV9hD/priscilla-du-preez-Njirpln-Vra8-unsplash.jpg'} alt="" />
-            <p className='text-2xl font-bold text-[#fee3e2] text-center -mt-20'>Developers</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={'https://i.ibb.co/0CSjmNV/agefis-qh-mar1-Tzo8-unsplash.jpg'} alt="" />
-            <p className='text-2xl font-bold text-[#fee3e2] text-center -mt-20'>Corporate professionals</p>
+    useEffect(() => {
+        AOS.init({ duration: 1000, once: true });
+    }, []);
 
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={'https://i.ibb.co/FJ6h2Sq/jason-goodman-m2-TU2gfq-Se-E-unsplash.jpg'} alt="" />
-            <p className='text-2xl font-bold text-[#fee3e2] text-center -mt-20'>Bankers</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={'https://i.ibb.co/1TybFBS/charlesdeluvio-r-RWi-VQz-Lm7k-unsplash.jpg'} alt="" />
-            <p className='text-2xl font-bold text-[#fee3e2] text-center -mt-20'>Agency</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={'https://i.ibb.co/5c29FDg/marissa-grootes-N9u-Or-BICcj-Y-unsplash.jpg'} alt="" />
-            <p className='text-2xl font-bold text-[#fee3e2] text-center -mt-20'>Workforce Planning</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={'https://i.ibb.co/FJ6h2Sq/jason-goodman-m2-TU2gfq-Se-E-unsplash.jpg'} alt="" />
-            <p className='text-2xl font-bold text-[#fee3e2] text-center -mt-20'>Employee Engagement</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={'https://i.ibb.co/0CSjmNV/agefis-qh-mar1-Tzo8-unsplash.jpg'} alt="" />
-            <p className='text-2xl font-bold text-[#fee3e2] text-center -mt-20'>HR Technology Solutions</p>
-          </SwiperSlide>
-          
-        </Swiper>
-      </section>
-      </>
+    const services = [
+        { title: 'Developers', image: 'https://i.ibb.co/t8wV9hD/priscilla-du-preez-Njirpln-Vra8-unsplash.jpg' },
+        { title: 'Corporate ', image: 'https://i.ibb.co/0CSjmNV/agefis-qh-mar1-Tzo8-unsplash.jpg' },
+        { title: 'Bankers', image: 'https://i.ibb.co/FJ6h2Sq/jason-goodman-m2-TU2gfq-Se-E-unsplash.jpg' },
+        { title: 'Agency', image: 'https://i.ibb.co/1TybFBS/charlesdeluvio-r-RWi-VQz-Lm7k-unsplash.jpg' },
+        { title: 'Workforce Planning', image: 'https://i.ibb.co/5c29FDg/marissa-grootes-N9u-Or-BICcj-Y-unsplash.jpg' },
+        { title: 'Employee ', image: 'https://i.ibb.co/FJ6h2Sq/jason-goodman-m2-TU2gfq-Se-E-unsplash.jpg' },
+        { title: 'HR Technology ', image: 'https://i.ibb.co/0CSjmNV/agefis-qh-mar1-Tzo8-unsplash.jpg' },
+    ];
+
+    return (
+        <motion.div
+            id="services"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            data-aos="fade-up"
+        >
+            <SectionTitle subheading={'---Find your service---'} heading={'Our Users '} />
+
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={10}
+                freeMode={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[FreeMode, Pagination]}
+                className="mySwiper"
+            >
+                {services.map((service, index) => (
+                   <SwiperSlide key={index}>
+                   <motion.div
+                       className="relative overflow-hidden"
+                       data-aos="fade-up"
+                       data-aos-delay={`${index * 100}`}
+                   >
+                       <img
+                           className="w-full h-72 object-cover rounded-lg transition-transform transform hover:scale-105"
+                           src={service.image}
+                           alt=""
+                       />
+                       <p className="text-2xl font-bold text-[#fcc2c0] text-center mt-4 ">
+                           {service.title}
+                       </p>
+                   </motion.div>
+               </SwiperSlide>
+               
+                
+                ))}
+            </Swiper>
+        </motion.div>
     );
 };
 
